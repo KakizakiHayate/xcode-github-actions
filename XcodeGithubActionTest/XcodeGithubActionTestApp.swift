040e7b9e21ab37474c39c9a734ef9126505fbbda
kakizaki hayate <hayate.h.0704@gmail.com>
2023-10-30 21:13:14 +0900

    Xcodeのイニシャルファイル追加

---
+++ XcodeGithubActionTest/XcodeGithubActionTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeGithubActionTestApp.swift
+//  XcodeGithubActionTest
+//
+//  Created by 柿崎逸 on 2023/10/30.
+//
+
+import SwiftUI
+
+@main
+struct XcodeGithubActionTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
